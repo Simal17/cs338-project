8fe74d62e651abe118b2700ffcf2454b81dc795e
Jothine Nandakumar <60521482+J0th1ne@users.noreply.github.com>
2024-06-11 12:27:13 -0400

    Added sample data into database

--- server.js
+++ server.js
@@ -14,28 +14,133 @@ app.listen(port, () => {
 const sqlite3 = require('sqlite3').verbose();
 let sql;
 
-//connect to DB
-const db = new sqlite3.Database('./test.db', sqlite3.OPEN_READWRITE, (err) => {
+//connection to DB
+const db = new sqlite3.Database('./sampletest.db', sqlite3.OPEN_READWRITE, (err) => {
     if (err) return console.error(err.message);
 });
 
-// create Table
-//sql = "CREATE TABLE student(uid DECIMAL(3, 0) NOT NULL PRIMARY KEY, name VARCHAR(30), score DECIMAL(3, 2))";
-//db.run(sql);
+// // USER TABLE
+// sql = "CREATE TABLE User(fname VARCHAR(30) NOT NULL, lname VARCHAR(30) NOT NULL, user_id INT NOT NULL PRIMARY KEY, pwd VARCHAR(30))";
+// db.run(sql);
 
-// insert data
-//sql = "INSERT INTO student VALUES(?,?,?)";
-//db.run(sql, ["1", "alice", "0.1"], (err) => {
- //   if (err) return console.error(err.message);
-//})
+// // read CSV files of data
+// const fs = require('fs');
+// const { parse } = require('csv-parse');
+// const { serialize } = require('v8');
 
-// query data
-app.get('/data', (req, res) => {
-    sql = "SELECT * FROM student";
-    db.all(sql, [], (err, rows) => {
-        if (err) {
-          throw err;
-        }
-        res.send(JSON.stringify(rows));
-      });
-  });
+// fs.createReadStream("./data/user-sample.csv")
+//   .pipe(parse({ delimiter: ",", from_line: 2}))
+//   .on("data", function(row) {
+//     db.serialize(function() {
+//       db.run(
+//         "INSERT INTO User VALUES (?,?,?,?)",
+//         [row[0], row[1], row[2], row[3]],
+//         function(error) {
+//           if(error) { return console.log(error.message);}
+//           console.log(`Inserted a row with the id: ${this.lastID}`)
+//         }
+//       );
+//     });
+//   })
+
+// .on("end", function(){
+//   console.log("finished");
+// })
+// .on("error", function(error){
+//   console.log(error.message);
+// });
+
+// // query data
+// // app.get('/data', (req, res) => {
+// //     sql = "SELECT * FROM Users";
+// //     db.all(sql, [], (err, rows) => {
+// //         if (err) {
+// //           throw err;
+// //         }
+// //         res.send(JSON.stringify(rows));
+// //       });
+// //   });
+
+
+// // PRODUCT TABLE
+// sql = "CREATE TABLE Product(model_no INT NOT NULL PRIMARY KEY, name VARCHAR(30) NOT NULL, manufacture VARCHAR(30) NOT NULL, retail_price DECIMAL(10, 2) NOT NULL, stock_qtty INT, tdp INT, form_factor VARCHAR(10))";
+// db.run(sql);
+
+// // read CSV files of data
+// fs.createReadStream("./data/product-sample.csv")
+//   .pipe(parse({ delimiter: ",", from_line: 2}))
+//   .on("data", function(row) {
+//     db.serialize(function() {
+//       db.run(
+//         "INSERT INTO Product VALUES (?,?,?,?,?,?,?)",
+//         [row[0], row[1], row[2], row[3], row[4], row[5], row[6]],
+//         function(error) {
+//           if(error) { return console.log(error.message);}
+//           console.log(`Inserted a row with the id: ${this.lastID}`)
+//         }
+//       );
+//     });
+//   })
+
+// .on("end", function(){
+//    console.log("finished");
+//  })
+//  .on("error", function(error){
+//    console.log(error.message);
+//  });
+
+
+// // PRODUCT ORDER TABLE
+// sql = "CREATE TABLE Order(order_id INT NOT NULL PRIMARY KEY, buyer VARCHAR(30) NOT NULL)";
+// db.run(sql);
+
+// // read CSV files of data
+// fs.createReadStream("./data/order-sample.csv")
+//   .pipe(parse({ delimiter: ",", from_line: 2}))
+//   .on("data", function(row) {
+//     db.serialize(function() {
+//       db.run(
+//         "INSERT INTO Product VALUES (?,?,?,?,?,?,?)",
+//         [row[0], row[1], row[2], row[3], row[4], row[5], row[6]],
+//         function(error) {
+//           if(error) { return console.log(error.message);}
+//           console.log(`Inserted a row with the id: ${this.lastID}`)
+//         }
+//       );
+//     });
+//   })
+
+// .on("end", function(){
+//    console.log("finished");
+//  })
+//  .on("error", function(error){
+//    console.log(error.message);
+//  });
+
+
+// // CPU TABLE
+// sql = "CREATE TABLE CPU(order_id INT NOT NULL PRIMARY KEY, buyer VARCHAR(30) NOT NULL)";
+// db.run(sql);
+
+// // read CSV files of data
+// fs.createReadStream("./data/order-sample.csv")
+//   .pipe(parse({ delimiter: ",", from_line: 2}))
+//   .on("data", function(row) {
+//     db.serialize(function() {
+//       db.run(
+//         "INSERT INTO Product VALUES (?,?,?,?,?,?,?)",
+//         [row[0], row[1], row[2], row[3], row[4], row[5], row[6]],
+//         function(error) {
+//           if(error) { return console.log(error.message);}
+//           console.log(`Inserted a row with the id: ${this.lastID}`)
+//         }
+//       );
+//     });
+//   })
+
+// .on("end", function(){
+//    console.log("finished");
+//  })
+//  .on("error", function(error){
+//    console.log(error.message);
+//  });
